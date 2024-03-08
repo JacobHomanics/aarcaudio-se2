c8107461b90e45dcaca9fae27a1a4c44f7a08ac8
Jacob Homanics <homanicsjake@gmail.com>
2024-03-08 04:09:43 -0600

    moved audio to bottom of screen

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -182,20 +182,26 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="items-center bg-slate m-1 p-1">
-          {tracks.length > 0 ? (
-            <Player
-              trackList={tracks}
-              includeTags={false}
-              includeSearch={false}
-              showPlaylist={false}
-              sortTracks={false}
-            />
-          ) : (
-            <></>
-          )}
+      <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
+        <div>
+          <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
+            <div></div>
+            {tracks.length > 0 ? (
+              <Player
+                trackList={tracks}
+                includeTags={false}
+                includeSearch={false}
+                showPlaylist={false}
+                sortTracks={false}
+              />
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
+      </div>
+
+      <div className="flex items-center flex-col flex-grow pt-10">
         <p className="text-primary-content text-2xl">BUY ALL THE SONGS IN THE COLLECTION TO CLAIM THE ALBUM COVER.</p>
         <NftCard nft={albumNft} buttonObj={albumBtnObj} />
         <div className="grid grid-cols-3 items-center bg-slate m-1 p-1">
