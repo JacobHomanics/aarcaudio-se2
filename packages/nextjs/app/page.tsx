c800798002dc0c7fd737b124881da1e6c847d0ae
Jacob Homanics <homanicsjake@gmail.com>
2024-03-10 07:28:03 -0500

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -329,7 +329,7 @@ const Home: NextPage = () => {
         {allNftsCards.length > 0 ? (
           <div className="grid grid-cols-3 items-center bg-slate m-1 p-1">{allNftsCards}</div>
         ) : (
-          <p className="text-primary-content text-2xl text-center">Seeking the Dark Truth...</p>
+          <p className="text-primary-content text-2xl text-center">Loading...</p>
         )}
       </div>
     </>
